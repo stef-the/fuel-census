4dc091697829c81708f17998cc9ff12b04c12edd
stef <stefthedoggo4@gmail.com>
2024-04-08 19:38:19 +0100

    fix deprecation warnings, add time remaining estimate

--- v1.py
+++ v1.py
@@ -3,9 +3,12 @@
 import pandas as pd
 import requests
 import json
+import time
+import datetime
+from cli_color_py import red, yellow, green, bold
 
 electric_stations_dataset = "electric_stations (Apr 7 2024).csv"
-date_search = "2023-01"  # format: yyyy-mm-dd (leave empty to scan all)
+date_search = "2023-02"  # format: yyyy-mm-dd (leave empty to scan all)
 
 skipped_sets = 0
 
@@ -14,15 +17,16 @@ benchmark = "Public_AR_Census2020"  # eg: "Public_AR_Census2020", "2020", etc.
 vintage = "Census2010_Census2020"
 
 # read charging/refueling stations dataset
-df = pd.read_csv(electric_stations_dataset)
+df = pd.read_csv(electric_stations_dataset, low_memory=False)
 
 # create a mask to filter by date
 mask = df["Open Date"].str.contains(str(date_search), case=False, na=False)
 masked_df = df[mask]
-masked_df["Qualify for Tax Benefits"] = "None"
+masked_df = masked_df.copy()
+masked_df.loc[:, "Qualify for Tax Benefits"] = "None"
 
 # read cross-check datasets and convert their FIPS codes to strings for later searching
-lowincome_df = pd.read_csv("Urban-Low-income-Communities-Dataset.csv")
+lowincome_df = pd.read_csv("Urban-Low-income-Communities-Dataset.csv", low_memory=False)
 NMTC_df = pd.read_excel(
     "NMTC_2016-2020_ACS_LIC_Sept1_2023.xlsb", engine="pyxlsb", header=0
 )
@@ -40,11 +44,13 @@ api_url = r"https://geocoding.geo.census.gov/geocoder/geographies/address?"
 space_encoding = lambda x: x.replace(" ", "+")
 
 total = masked_df.shape[0]
-done = 0
+done, total_time = 0, 0
 print(str(total) + " data points")
 print("Running for " + date_search)
+
 # iter through each filtered charging/refueling station
 for index, row in masked_df.iterrows():
+    start_time = time.time()
     args = [
         "street=" + space_encoding(str(row["Street Address"])),
         "city=" + space_encoding(str(row["City"])),
@@ -59,7 +65,6 @@ for index, row in masked_df.iterrows():
     # eg geocoder/geographies/onelineaddress?address=4600+silver+hill+rd%2C+20233&benchmark=Public_AR_Census2020&vintage=Census2010_Census2020&format=json
     request_api = api_url + "&".join(args)
     done += 1
-    print("\n" + str(int((done/total)*100)) + "% " + str(done) + "/" + str(total) + " " + request_api)
 
     # retrieve data from the api
     response = requests.get(request_api)
@@ -97,8 +102,11 @@ for index, row in masked_df.iterrows():
                                 "Does Census Tract Qualify For NMTC Low-Income Community (LIC) on Poverty or Income Criteria?"
                             ].values
 
-                            masked_df["Qualify for Tax Benefits"][index] = (
-                                True if case1 == ["yes"] or case2 == ["YES"] else False
+                            masked_df.loc[:, ("Qualify for Tax Benefits", index)] = (
+                                True
+                                if (len(case1) > 0 and case1[0] == "yes")
+                                or (len(case2) > 0 and case2[0] == "YES")
+                                else False
                             )
 
                         else:
@@ -113,6 +121,21 @@ for index, row in masked_df.iterrows():
     except json.decoder.JSONDecodeError as e:
         print("Error decoding JSON: " + str(e))
         skipped_sets += 1
+
+    time_delta = time.time() - start_time
+    total_time += time_delta
+    percentage_complete = int((done / total) * 100)
+
+    print(
+        (red(str(percentage_complete)) if percentage_complete < 33 else yellow(str(percentage_complete)) if percentage_complete < 67 else green(str(percentage_complete)))
+        + "% - "
+        + str(done)
+        + "/"
+        + str(total)
+        + " - Estimate time remaining (h/m/s): "
+        + bold(str(datetime.timedelta(seconds=total_time / done * (total - done))))
+        + "\n"
+    )
 
 csv_data = masked_df.to_csv("output.csv", index=True)
 print("Skipped sets: " + str(skipped_sets))
